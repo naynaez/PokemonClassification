24edeb528dca90ff1fdd15d7d5f2545c146047a8
Peerawat Pipattanakulchai <peerawatpipattanakulchai@Peerawats-MacBook-Pro.local>
2018-03-25 01:43:58 +0700

    Task: Handle missing value

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-usedAttributes = ["type1", "defense", "capture_rate", "sp_defense"]  # Define selected attributes.
+usedAttributes = ["type1", "type2", "capture_rate", "sp_defense"]  # Define selected attributes.
 pokemonData = pd.read_csv('pokemon.csv', usecols=usedAttributes)  # Read CSV file.
 
     # Replace NaN object with "NULL" string
@@ -10,7 +10,6 @@ for i in range(usedAttributes.__len__()):
     for j in range(pokemonData.__len__()):
         if pokemonData[usedAttributes[i]][j] is pd.np.nan:
             pokemonData[usedAttributes[i]][j] = "NULL"
-
 
 # for i in range(4):      # Data Visualization
 #     if( i != 0):
@@ -20,7 +19,13 @@ for i in range(usedAttributes.__len__()):
 #         plt.show()
 
 ### Convert type string to type number
-pokemonData['type1_label'] = pokemonData
+#type1Value = dict(zip(pokemonData['type1'].astype('category').values, range(18)))
+type1_value = dict(zip(pokemonData['type1'].astype('category').cat.categories.tolist(), range(18)))
+type2_value = dict(zip(pokemonData['type2'].astype('category').cat.categories.tolist(), range(19)))
+
+pokemonData['type1_encoded'] = pokemonData['type1'].map(type1_value)
+pokemonData['type2_encoded'] = pokemonData['type2'].map(type2_value)
+print(pokemonData['type2_encoded'])
 
 ### Logistic Regression Classification
 from sklearn.linear_model import LogisticRegression
@@ -29,8 +34,8 @@ from sklearn import metrics
 clf = LogisticRegression()
 X = pokemonData[[usedAttributes[1], usedAttributes[2], usedAttributes[3]]]
 y = pokemonData[usedAttributes[0]]
-clf.fit(X,y)
-
-coeff_df = pd.DataFrame(clf.coef_, X.columns, columns=['Coefficient'])
-print(coeff_df)
-print("Intercept = " + str(clf.intercept_))
+clf.fit(X, y)
+#
+# coeff_df = pd.DataFrame(clf.coef_, X.columns, columns=['Coefficient'])
+# print(coeff_df)
+# print("Intercept = " + str(clf.intercept_))
